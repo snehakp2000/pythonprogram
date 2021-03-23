b8f6c404d77578786d3b0277094084b40b6ace98
snehakp2000 <snehakallullaparambu@gmail.com>
2021-03-24 01:37:41 +0530

    commit 30

---
+++ co2prg11.py
@@ -0,0 +1,6 @@
+square=lambda a:a*a
+print("area of a square is",square(4))
+rect=lambda a,b:a*b
+print("area of rectangle is",rect(4,6))
+tri=lambda b,h:0.5*b*h
+print("area of triangle ",tri(4,6))
